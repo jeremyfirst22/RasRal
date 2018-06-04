67114023134cdbb3adbf6aee09250306b042f2ca
jeremyfirst22 <jeremy_first@utexas.edu>
2018-06-04 10:25:13 -0500

    Initial commit. run.sh script adopted and stripped from GFP_Hbond project. mdp_files copied from GFP_Hbond

---
+++ run_RasRas_GTP_Validation.sh
@@ -0,0 +1,275 @@
+#!/bin/bash
+
+usage(){
+    echo "USAGE: $0 <PDB file {molec.pdb} >"  
+    exit 
+}
+
+if [ -z $1 ] ; then 
+    usage 
+    fi 
+
+fileName=$1 
+if [ ! -f $fileName ] ; then 
+    echo "ERROR: $fileName not found " 
+    exit 
+    fi 
+if [[ $fileName == *.pdb ]] ; then 
+    MOLEC=$(basename $fileName)
+    MOLEC=${MOLEC%.*}
+else 
+    echo "ERROR: Input file must be PDB file (*.pdb)" 
+    exit 
+    fi 
+if [ ! -d $MOLEC ] ; then mkdir $MOLEC ; fi 
+if [ ! -f $MOLEC/$fileName ] ; then cp $fileName $MOLEC/. ; fi 
+
+TOP=${PWD}
+MDP=$TOP/mdp_files
+logFile=$TOP/$MOLEC/$MOLEC.log 
+errFile=$TOP/$MOLEC/$MOLEC.err 
+FF=$TOP/GMXFF
+forceField=amber03
+FORCE_TOOLS=/Users/jfirst/force_calc_tools
+if [ ! -d $FF/$forceField.ff ] ; then 
+    echo ; echo "ERROR: FF not found" 
+    exit
+    fi  
+
+check(){
+    for arg in $@ ; do  
+         if [ ! -s $arg ] ; then 
+             echo ; echo "ERROR: $arg missing. Exitting" 
+             exit 
+             fi  
+         done 
+}
+
+clean(){
+    if [ -d $forceField.ff ] ; then rm -r $forceField.ff *.dat ; fi  
+}
+
+create_dir(){
+    if [ -z $1 ] ; then 
+        echo "ERROR: create_dir requires argument. " ; exit ; fi  
+
+    dirName=$1 
+    if [ ! -d $dirName ] ; then mkdir $dirName ; fi  
+                                                        
+    if [ ! -d $dirName/$forceField.ff ] ; then 
+        if [ -d $FF/$forceField.ff ] ; then 
+            cp -r $FF/$forceField.ff $dirName
+            cp $FF/*.dat $dirName/. 
+        else 
+            echo "FF not found" 
+            exit 
+            fi  
+        fi  
+}
+
+protein_steep(){
+    printf "\t\tProtein steep............................." 
+    if [ ! -f Protein_steep/protein_steep.gro ] ; then 
+        create_dir Protein_steep
+        
+        cp $MOLEC.pdb Protein_steep/.
+        cd Protein_steep
+
+        gmx pdb2gmx -f $MOLEC.pdb \
+            -p $MOLEC.top \
+            -ff $forceField \
+            -water tip3p \
+            -merge all \
+            -o $MOLEC.gro >> $logFile 2>> $errFile 
+        check $MOLEC.gro 
+
+        echo 'Backbone' | gmx editconf -f $MOLEC.gro \
+            -d 1.5 \
+            -bt dodecahedron \
+            -o boxed.gro >> $logFile 2>> $errFile
+        check boxed.gro 
+
+        gmx grompp -f $MDP/protein_steep.mdp \
+            -c boxed.gro \
+            -p $MOLEC.top \
+            -o protein_steep.tpr >> $logFile 2>> $errFile 
+        check protein_steep.tpr 
+
+        gmx mdrun -deffnm protein_steep >> $logFile 2>> $errFile 
+        check protein_steep.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi
+} 
+
+solvate(){
+    printf "\t\tSolvating protein........................." 
+    if [ ! -f Solvate/neutral.top ] ; then 
+        create_dir Solvate
+        
+        cp Protein_steep/protein_steep.gro Solvate/. 
+        cp Protein_steep/$MOLEC.top Solvate/. 
+        cp Protein_steep/*.itp Solvate/. 
+        cd Solvate
+
+        gmx solvate -cp protein_steep.gro \
+            -p $MOLEC.top \
+            -o solvated.gro >> $logFile 2>> $errFile 
+        check solvated.gro
+
+        gmx grompp -f $MDP/vac_md.mdp \
+            -p $MOLEC.top \
+            -c solvated.gro \
+            -o genion.tpr >> $logFile 2>> $errFile 
+        check genion.tpr
+        
+        cp $MOLEC.top neutral.top
+        echo 'SOL' | gmx genion -s genion.tpr \
+            -neutral \
+            -nname 'CL' \
+            -pname 'NA' \
+            -p neutral.top \
+            -o neutral.gro >> $logFile 2>> $errFile 
+        check neutral.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi  
+} 
+
+solvent_steep(){
+    printf "\t\tSolvent steep............................." 
+    if [ ! -f Solvent_steep/solvent_steep.gro ] ; then 
+        create_dir Solvent_steep
+        
+        cp Solvate/neutral.gro Solvent_steep/. 
+        cp Solvate/neutral.top Solvent_steep/. 
+        #cp Solvate/neutral*.itp Solvent_steep/. 
+        cp Solvate/posre*.itp Solvent_steep/. 
+        cd Solvent_steep
+
+        gmx grompp -f $MDP/solvent_steep.mdp \
+            -p neutral.top \
+            -c neutral.gro \
+            -o solvent_steep.tpr >> $logFile 2>> $errFile 
+        check solvent_steep.tpr 
+
+        gmx mdrun -deffnm solvent_steep >> $logFile 2>> $errFile 
+        check solvent_steep.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi  
+}
+
+solvent_nvt(){
+    printf "\t\tSolvent NVT relaxation...................." 
+    if [ ! -f Solvent_nvt/solvent_nvt.gro ] ; then 
+        create_dir Solvent_nvt
+        
+        cp Solvent_steep/solvent_steep.gro Solvent_nvt/. 
+        cp Solvent_steep/neutral.top Solvent_nvt/. 
+        cp Solvent_steep/*.itp Solvent_nvt/. 
+        cd Solvent_nvt
+
+        gmx grompp -f $MDP/solvent_nvt_relax.mdp \
+            -c solvent_steep.gro \
+            -p neutral.top \
+            -o solvent_nvt.tpr >> $logFile 2>> $errFile 
+        check solvent_nvt.tpr 
+
+        gmx mdrun -deffnm solvent_nvt >> $logFile 2>> $errFile 
+        check solvent_nvt.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi  
+}
+
+solvent_npt(){
+    printf "\t\tSolvent NPT relaxation...................." 
+    if [ ! -f Solvent_npt/solvent_npt.gro ] ; then 
+        create_dir Solvent_npt
+        
+        cp Solvent_nvt/solvent_nvt.gro Solvent_npt/. 
+        cp Solvent_nvt/neutral.top Solvent_npt/. 
+        cp Solvent_nvt/*.itp Solvent_npt/. 
+        cd Solvent_npt
+
+        gmx grompp -f $MDP/solvent_npt_relax.mdp \
+            -c solvent_nvt.gro \
+            -p neutral.top \
+            -o solvent_npt.tpr >> $logFile 2>> $errFile 
+        check solvent_npt.tpr 
+
+        gmx mdrun -deffnm solvent_npt >> $logFile 2>> $errFile 
+        check solvent_npt.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi  
+}
+
+production(){
+    printf "\t\tProduction run............................" 
+    if [ ! -f Production/$MOLEC.nopbc.gro ] ; then 
+        create_dir Production
+        
+        cp Solvent_npt/neutral.top Production/.
+        cp Solvent_npt/solvent_npt.gro Production/.
+        cp Solvent_npt/*.itp Production/. 
+        cd Production
+
+        if [ ! -f $MOLEC.gro ] ; then 
+            if [ ! -f $MOLEC.tpr ] ; then 
+                gmx grompp -f $MDP/production.mdp \
+                    -p neutral.top \
+                    -c solvent_npt.gro \
+                    -o $MOLEC.tpr >> $logFile 2>> $errFile 
+                fi 
+                check $MOLEC.tpr 
+
+            if [ -f $MOLEC.cpt ] ; then 
+                ibrun mdrun_mpi -deffnm $MOLEC \
+                    -cpi $MOLEC.cpt >> $logFile 2>> $errFile  
+            else 
+                ibrun mdrun_mpi -deffnm $MOLEC >> $logFile 2>> $errFile 
+                fi 
+            fi 
+        check $MOLEC.gro 
+
+        clean
+        printf "Success\n" 
+        cd ../
+    else
+        printf "Skipped\n"
+        fi  
+} 
+
+printf "\n\t\t*** Program Beginning ***\n\n" 
+cd $MOLEC
+protein_steep
+solvate
+solvent_steep
+solvent_nvt
+solvent_npt
+production 
+cd ../
+
+printf "\n\n\t\t*** Program Ending    ***\n\n" 
